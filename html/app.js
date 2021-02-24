e85c5a665ba03a746854feb10cb7b3ff2aa8b623
Shivani Saini <49878058+Shivani-saini@users.noreply.github.com>
2021-02-24 22:11:39 +0530

    Add files via upload

---
+++ html/app.js
@@ -0,0 +1,20 @@
+var myApp = angular.module("myList", []);
+myApp.controller("myListController", function($scope) {
+	
+	$scope.items = [];
+	$scope.newItem ="";
+	$scope.pushItem = function (){
+		if ($scope.newItem != "") {
+			$scope.items.push($scope.newItem);
+			$scope.newItem = "";
+
+		}
+	}
+    $scope.deleteItem = function(index){
+    	$scope.items.splice(index, 1);
+
+    }
+  
+}); 
+
+
